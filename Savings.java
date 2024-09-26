05ea99c04595a22bcca53a59d960e64ba69b75aa
prietoc2022 <116029971+prietoc2022@users.noreply.github.com>
2024-09-25 22:59:13 -0600

    Update Savings.java

--- Savings.java
+++ Savings.java
@@ -1,4 +1,4 @@
-public class Savings extends Account{
+public class Savings extends ACCOUNT{
     public Savings(String username, int accountNumber, double accountBalance) {
         super(username, accountNumber, accountBalance);
 
